00804fc933c69b7d6520dff281040479626b4be8
Afaan Bilal <afaanbilal@gmail.com>
2024-01-18 09:23:02 +0530

    ✨ Default to dark mode

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,6 +51,9 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
